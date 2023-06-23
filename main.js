6717d32292ee8ec9b67bac0a7c72928212dbf0d7
behzodbaxtiyorov <behzodbaxtiyorov6@gmail.com>
2023-06-23 09:44:27 +0500

    vazifa

---
+++ main.js
@@ -0,0 +1,88 @@
+// var a = 7;
+// var result = a>0 ? "true" : "false";
+// console.log(result);
+
+
+// var a = 4;
+// var result = a%2==0 ? "juft son" : "juft son emas";
+// console.log(result);
+
+// var a = 4;
+// var result = a%2!=0 ? "juft son" : "juft son emas";
+// console.log(result);
+
+
+// var a = 7;
+// var b = 5;
+// var result = a>0 && b>0 ? "true" : "false";
+// console.log(result);
+
+
+// var a = 9;
+// var b = 5;
+// var c = 4;
+// var result = a>b && b>c ? "true" : "false";
+// console.log(result);
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+// var result = a<b && b<c ? "true" : "false";
+// console.log(result);
+
+
+// var a = 3;
+// var b = 2;
+// var result = a % 2 !=0 && b % 2 !=0 ? "true" : "false";
+// console.log(result);
+
+// var a = 3;
+// var b = 2;
+// var c = 5;
+// var d = 8;
+// var result = a>0 && b>0 && c>0 && d>0 ? "true" : "false";
+// console.log(result);
+
+
+// var a = 3;
+// var b = 6;
+// var c = 0;
+// var result = a>0 && b>0 || b>0 && c>0 || a>0 && c>0 ? "true" : "false";
+// console.log(result);
+
+
+// var day = 1;
+
+// var result = day ==1 ? "monday" : "false";
+// console.log(result);
+
+
+
+// let k = 5; n = 10;
+ 
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(k);
+// }
+
+
+
+
+
+// let a = 8;
+// let b = 15;
+// for (let i = a; i<= b; i++){
+//   console.log(i);
+// }
+
+
+
+
+// let a = 10, b = 2;
+
+
+// if (a > b) {
+//     for (let i = a - 1; i > b; i--) {
+//         console.log(i);
+//       }
+// }
